4d14aaba213263424dc462f03cad7ce8ed523131
“JBTOC” <numberonepunter@yahoo.com>
2024-04-24 15:08:31 +0100

    PrivateKeys.py added

--- AIPFixturesRequest.py
+++ AIPFixturesRequest.py
@@ -1,6 +1,9 @@
 import pip._vendor.requests as requests
+from .PrivateKeys import my_API_Key
 
-my_API_Key = "d0ced636d8212c0076ca83feb0b7dc11"
+
+# my_API_Key = PrivateKeys.my_API_Key
+
 
 headers = {
     'x-rapidapi-host': "https://v3.football.api-sports.io/",
@@ -11,6 +14,15 @@ headers = {
 url = "https://v3.football.api-sports.io/fixtures"
 
 #return jsonFixtures
+##
+# The function fixtureListForRound takes in three parameters: 
+# season, league, and round. 
+# It uses these parameters to construct a request URL using the requests library.
+# The request is made to the NHL (Wrong - its football ie Premier League) API, 
+# which returns a JSON object containing information about the fixtures for the specified round.
+# Finally, the function returns the JSON object.
+##
+
 def fixtureListForRound (season, league, round):
     roundNumAsString = f'Regular Season - {round}'
     params = {'season': season, 'league': league, 'round': roundNumAsString}
